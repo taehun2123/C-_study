af55a7288d16050d4a279ed9f37373864cb4358f
taehun2123 <pyoneng_@naver.com>
2024-03-14 11:40:33 +0900

    자료형 정리, 정수, 실수형에 대한 표현 차이 비교, 에러 비교

---
+++ study1/ConsoleApp3/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 123;
+        int b = 0b1111;
+        int c = 0xff;
+        int i1 = 2_147_483_647; //실행됨
+        long l1 = 1234567890123456789L;
+        double ad = 3.14D;
+        float bd = 3.14F;
+        decimal d = 3.14M;
+        double cd = 3.14;
+        //float f1 = 3.14; //오류 
+        //decimal f2 = 3.14; //오류
+        double d1 = 3.14F;
+        float f3 = 100;
+        double f4 = 100;
+        System.Int32 iVal = 123;
+        Console.WriteLine("컴퓨터IT학부 2312008 김태훈");
+        Console.WriteLine(a);
+        Console.WriteLine(iVal);
+        Console.WriteLine(b);
+        Console.WriteLine(c);
+        Console.WriteLine(i1);
+        Console.WriteLine(l1);
+        Console.WriteLine(d);
+
+        const int kk = 100;
+        //kk = 200; err
+    }
+}
--- study1/ConsoleApp3/obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
+++ study1/ConsoleApp3/obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a3e34c65acc075b516d27500b1ea53ce98e89bd")]
-[assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
+[assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp3")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment 클래스에서 생성되었습니다.
